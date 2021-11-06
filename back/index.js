e46cbfcce51bcb6d126d885242363989daa4b1df
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-06 20:25:04 +0900

    husky test

--- back/index.js
+++ back/index.js
@@ -11,12 +11,11 @@ const morgan = require('morgan');
 const path = require('path');
 const redis = require('redis');
 const RedisStore = require('connect-redis')(session);
-const logger = require("./logger");
+const logger = require('./logger');
 
 dotenv.config();
 const redisClient = redis.createClient({
-
-      url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
   password: process.env.REDIS_PASSWORD,
 });
 const userRouter = require('./routes/user');
@@ -25,7 +24,8 @@ const passportConfig = require('./passport');
 
 const app = express();
 const port = 5000;
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
     console.log('db 연결 성공');
   })
@@ -37,16 +37,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
   app.use(helmet({ contentSecurityPolicy: false }));
   app.use(hpp());
-  app.use(cors({
-    origin: '',   // 배포시 프론트 주소 적어주기
-    credentials: true,
-  }));
+  app.use(
+    cors({
+      origin: '', // 배포시 프론트 주소 적어주기
+      credentials: true,
+    }),
+  );
 } else {
   app.use(morgan('dev'));
-  app.use(cors({
-    origin: true,
-    credentials: true,
-  }));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
 }
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
@@ -76,7 +80,7 @@ app.use('/user', userRouter);
 
 // 404처리 미들웨어
 app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
   logger.info('hello');
   logger.error(error.message);
