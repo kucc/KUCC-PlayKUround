5a32b1ca84246d805a796ac8f89d4c206ddc1fc6
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-15 00:46:04 +0900

    babelrc 삭제 후 babel.config.js 내용 작성

---
+++ front/babel.config.js
@@ -0,0 +1,35 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['next/babel'],
+    plugins: [
+      [
+        'babel-plugin-styled-components',
+        {
+          ssr: true,
+          displayName: true,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@components': './components',
+            '@containers': './containers',
+            '@constants': './constants',
+            '@config': './config',
+            '@assets': './assets',
+            '@hooks': './hooks',
+            '@styles': './styles',
+            '@util': './util',
+            '@pages': './pages',
+            '@store': './store',
+            '@reducers': './reducers',
+            '@sagas': './sagas',
+          },
+        },
+      ],
+    ],
+  };
+};
