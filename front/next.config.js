938af5df7e6473edcde6fb7e8c24fe69be4807da
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-18 22:24:03 +0900

    Antd Customizing problem solve (temporary)

--- front/next.config.js
+++ front/next.config.js
@@ -1,71 +1,41 @@
-// TODO: Antd Customizing 위한 설정 마치기
-require('dotenv').config();
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
-const withImages = require('next-images');
-const withSass = require('@zeit/next-sass');
-const withLess = require('@zeit/next-less');
-const withPlugins = require('next-compose-plugins');
+const withAntdLess = require('next-plugin-antd-less');
 
-// front\node_modules\antd\dist\antd.less
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
+module.exports = withAntdLess({
+  // optional: you can modify antd less variables directly here
+  modifyVars: { '@primary-color': 'rgb(255, 0, 212)' },
+  // Or better still you can specify a path to a file
+  lessVarsFilePath: './styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
 
-// front\node_modules\antd\dist\antd.less
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, 'less/antd-custom.less'), 'utf8'),
-);
+  // Other Config Here...
 
-const plugins = [
-  withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables,
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        loader: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  options: {
+    presets: ['@babel/preset-react'],
+  },
+  // ONLY for Next.js 10, if you use Next.js 11, delete this block
+  webpack(config) {
+    return config;
+  },
+  plugins: [
+    require('@babel/plugin-proposal-decorators').default,
+    {
+      legacy: true,
     },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-        ];
-
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader',
-        });
-      }
-      return config;
-    },
-  }),
-  withSass,
-];
-module.exports = withPlugins(plugins);
-// module.exports = withBundleAnalyzer({
-//   compress: true,
-//   webpack(config, { webpack }) {
-//     const prod = process.env.NODE_ENV === 'production';
-//     return {
-//       ...config,
-//       mode: prod ? 'production' : 'development',
-//       devtool: prod ? 'hidden-source-map' : 'eval',
-//       plugins: [
-//         ...config.plugins,
-//         // 다른 쓸데없는 언어 없애주기
-//         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
-//       ],
-//     };
-//   },
-// });
-
-module.exports = withImages();
+  ],
+});
