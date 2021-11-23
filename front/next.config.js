f0178d11c5215a53c1dcd4c505d8fc57a053a878
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-23 21:31:16 +0900

    Fix vscode can't Find less File

---
+++ front/next.config.js
@@ -0,0 +1,43 @@
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // optional: you can modify antd less variables directly here
+  // modifyVars: {
+  //   // '@primary-color': 'rgb(255, 0, 212)',
+  // },
+  // Or better still you can specify a path to a file
+  lessVarsFilePath: './styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        loader: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  options: {
+    presets: ['@babel/preset-react'],
+  },
+  // ONLY for Next.js 10, if you use Next.js 11, delete this block
+  webpack(config) {
+    return config;
+  },
+  plugins: [
+    require('@babel/plugin-proposal-decorators').default,
+    {
+      legacy: true,
+    },
+  ],
+});
