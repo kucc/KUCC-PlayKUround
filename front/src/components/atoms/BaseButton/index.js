5eb4ad15293161670076e25424f4688868ca8b8c
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-12-20 23:57:18 +0900

    BaseButton isComplete & disabled 제거

--- front/src/components/atoms/BaseButton/index.js
+++ front/src/components/atoms/BaseButton/index.js
@@ -4,22 +4,15 @@ import PropTypes from 'prop-types';
 
 import { ButtonComplete } from './styled';
 
-export const BaseButton = ({ isComplete, children, htmlType, ...props }) => {
+export const BaseButton = ({ children, htmlType, ...props }) => {
   return (
-    <ButtonComplete
-      disabled={!isComplete}
-      htmlType={htmlType}
-      isComplete={isComplete}
-      type='primary'
-      {...props}
-    >
+    <ButtonComplete htmlType={htmlType} type='primary' {...props}>
       {children}
     </ButtonComplete>
   );
 };
 
 BaseButton.propTypes = {
-  isComplete: PropTypes.bool.isRequired,
   children: PropTypes.any.isRequired,
-  htmlType: 'button' | 'submit' | 'reset',
+  htmlType: 'button' | 'submit' | 'reset'.isRequired,
 };
