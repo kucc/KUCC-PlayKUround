d6ee7ba9fe24f1d1b1baff86b9a3c7a783f75553
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-01-24 11:42:13 +0900

    /api/ 추가

--- front/apis/user.ts
+++ front/apis/user.ts
@@ -6,17 +6,17 @@ axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 
 export function loadMyInfoAPI() {
-  return axios.get('/user').then(response => response.data);
+  return axios.get('/api/user').then(response => response.data);
 }
 
 export function logInAPI(data: { email: string; password: string }) {
-  return axios.post('/user/login', data).then(response => response.data);
+  return axios.post('/api/user/login', data).then(response => response.data);
 }
 
 export function logOutAPI() {
-  return axios.post('/user/logout').then(response => response.data);
+  return axios.post('/api/user/logout').then(response => response.data);
 }
 
 export function registerAPI(data: { email: string; name: string; password: string }) {
-  return axios.post('/user/register', data).then(response => response.data);
+  return axios.post('/api/user/register', data).then(response => response.data);
 }
