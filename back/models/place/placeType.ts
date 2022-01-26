d1b203f90e7c23d67e655aaa801fd4f70719735d
LHJ <jjs01hwang@gmail.com>
2022-01-27 02:48:35 +0900

    swagger 추가 및 ts-node hot reloading 추가

--- back/models/place/placeType.ts
+++ back/models/place/placeType.ts
@@ -1,44 +1,49 @@
-import { Model, Optional } from "sequelize/types";
+import { Model, Optional } from 'sequelize/types';
 
 interface menuType {
-  menu_name : string;
-  menu_price : number;
-  menu_picture : string[]
+  menu_name: string;
+  menu_price: number;
+  menu_picture: string[];
 }
 
 export interface PlaceAttributes {
-  id? : number;
+  id?: number;
   // 위도, 경도
-  address_location : number[]
+  address_location: number[];
   // 정확한 좌표
-  address_exact : string;
-  address_category : "혜화" | "안암" | "성신여대";
-  picture_link : string[];
-  place_Name : string
-  place_Time : number[]
-  place_phoneNum : string;
-  naver_link : string
-  catch_table_link : string;
-  insta_link : string
-  place_hashtag : string[]
-  place_category : string
-  place_category_detail : number
-  place_price : "free" | "low" | "mid"
+  address_exact: string;
+  address_category: '혜화' | '안암' | '성신여대';
+  picture_link: string[];
+  place_Name: string;
+  place_Time: string[];
+  place_phoneNum: string;
+  naver_link: string;
+  catch_table_link: string;
+  insta_link: string;
+  place_hashtag: string[];
+  place_category: string;
+  place_category_detail: number;
+  place_price: 'free' | 'low' | 'mid';
   // 메뉴는 따로 모델 만들어서 연결시켜 줘야 할듯
   // recommended_menu : menuType
   // all_menu : menuType[]
-  scrab_count : number
-  date_concept : "동적" | "비동적" | "체험",
-  writer: string
+  scrab_count: number;
+  date_concept: '동적' | '비동적' | '체험';
+  writer: number;
 }
 
+interface PlaceCreationAttributes
+  extends Optional<
+    PlaceAttributes,
+    | 'id'
+    | 'picture_link'
+    | 'place_Time'
+    | 'place_phoneNum'
+    | 'naver_link'
+    | 'catch_table_link'
+    | 'place_hashtag'
+  > {}
 
-interface PlaceCreationAttributes extends Optional<PlaceAttributes, "id"| "picture_link" |
-"place_Time" |
-"place_phoneNum" |
-"naver_link" |
-"catch_table_link" |
-"place_hashtag" > {}
-
-export interface PlaceInterface extends Model<PlaceAttributes, PlaceCreationAttributes>,
-PlaceAttributes {}
+export interface PlaceInterface
+  extends Model<PlaceAttributes, PlaceCreationAttributes>,
+    PlaceAttributes {}
--- back/routes/user.ts
+++ back/routes/user.ts
@@ -1,41 +1,78 @@
 const express = require('express');
-const controller = require("../controllers/user.controller");
+const controller = require('../controllers/user.controller');
 
 const { isLoggedIn } = require('../middlewares/Auth');
 
 const router = express.Router();
-
-
+// swagger - paths:로 한번에 모아줄 수도 있다..
 
 // eslint-disable-next-line consistent-return
 /**
  * @swagger
  * /api/user/:
- *  tags:
- *    -user
  *  get:
- *    description : 유저 조회
- *    produces: 
- *      - application/json
+ *    tags:
+ *      - user
+ *    summary : 유저 정보 가져오기
+ *    description : Get 방식으로 유저의 정보를 가져옵니다.
  *    responses:
  *      401:
  *        description : 로그인이 필요합니다.
  *      200:
  *        description : 유저 조회 성공
- *    
+ *
  */
 router.get('/', isLoggedIn, controller.userGet);
 
+/**
+ * @swagger
+ * /api/user/register:
+ *  post:
+ *    tags:
+ *      - user
+ *    summary : 유저 회원가입
+ *    description : Post 방식으로 유저를 로그인합니다.
+ *    requestBody:
+ *      description: 유저 회원가입입니다.
+ *      required: true
+ *      content:
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - name
+ *              - email
+ *              - password
+ *            properties:
+ *              name:
+ *                type: string
+ *                description: 유저 이름입니다.
+ *                example : Hjun
+ *              email:
+ *                type: string
+ *                description: 유저 이메일입니다.
+ *                example : jjs05hwang@gmail.com
+ *              password:
+ *                type: string
+ *                description: 유저 비밀번호입니다.
+ *                example : 1234
+ *    responses:
+ *      200:
+ *        description : 로그인 성공
+ *      401:
+ *        description : 인증 실패.
+ *      403:
+ *        description : 이미 사용중인 이메일입니다..
+ *
+ */
 router.post('/register', controller.userRegister);
-
-
 
 /**
  * @swagger
  * /api/user/login:
- *  tags:
- *    -user
  *  post:
+ *    tags:
+ *      - user
  *    summary : 유저 로그인
  *    description : Post 방식으로 유저를 로그인합니다.
  *    requestBody:
@@ -48,18 +85,18 @@ router.post('/register', controller.userRegister);
  *            properties:
  *              email:
  *                type: string
- *                description: "유저 이메일"
+ *                description: 유저 이메일입니다.
  *                example : jjs05hwang@gmail.com
  *              password:
  *                type: string
- *                description: "유저 비밀번호"
+ *                description: 유저 비밀번호입니다.
  *                example : 1234
  *    responses:
  *      401:
  *        description : 인증 실패.
  *      200:
  *        description : 로그인 성공
- *    
+ *
  */
 router.post('/login', controller.userLogin);
 
