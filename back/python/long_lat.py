ee5a73ec2df08c9c06614f6c94267164f1e9f195
xuio-0528 <aion5467@gmail.com>
2022-02-02 13:52:11 +0900

    csv export
    이제 애들 시켜야지

--- back/python/long_lat.py
+++ back/python/long_lat.py
@@ -1,11 +1,18 @@
 import requests
+import pandas as pd
 
-data = pd.read_csv("./sorted_playkuroud.csv")
-
-searching = data.iloc[0]
-url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
-headers = {
-    "Authorization": "ea1128b9fb01a06cc80760f350042441"
-}
-places = requests.get(url, headers = headers).json()['documents']
-places
+data = pd.read_csv("./sorted_playkuround.csv")
+for i in range(data.shape[0]):
+    searching = data['이름'].iloc[i]
+    url = 'https://dapi.kakao.com/v2/local/search/keyword.json?x=37.588194705681&y=127.03402453668&query={}'.format(searching)
+    headers = {
+        "Authorization": "KakaoAK ea1128b9fb01a06cc80760f350042441"
+    }
+    try : 
+        data['위도'].iloc[i] = requests.get(url, headers = headers).json()['documents'][0]['y']
+        data['경도'].iloc[i] = requests.get(url, headers = headers).json()['documents'][0]['x']
+        data['전화번호'].iloc[i] = requests.get(url, headers = headers).json()['documents'][0]['phone']
+        data['주소'].iloc[i] = requests.get(url, headers = headers).json()['documents'][0]['address_name']
+    except IndexError: 
+        pass
+data.to_csv("temp_playkuround.csv", mode='w')
