db0078248aebe93b3ab921672f3025a221556220
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-05 22:46:22 +0900

    InstaCard 컴포넌트 제작

---
+++ front/src/components/molecules/InstaCard/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import { Chip } from '@components';
+
+import {
+  CardHeadContainer,
+  ChipContainer,
+  Container,
+  Description,
+  Place,
+  StyledImg,
+  TextContainer,
+  Title,
+} from './styled';
+import { InstaCardProps } from './type';
+
+export const InstaCard = ({
+  titleText,
+  placeText,
+  icon,
+  clickedIcon,
+  label,
+  descriptionText,
+}: InstaCardProps) => {
+  const [clicked, setIsClicked] = useState<boolean>(false);
+  const onClick = () => {
+    setIsClicked(!clicked);
+  };
+
+  return (
+    <Container>
+      <CardHeadContainer>
+        <TextContainer>
+          <Title>{titleText}</Title>
+          <Place>{placeText}</Place>
+        </TextContainer>
+        <ChipContainer>
+          <Chip
+            nonClickedIcon={icon}
+            clickedIcon={clickedIcon}
+            label={label}
+            shadow={true}
+            onClick={onClick}
+            clicked={clicked}
+          />
+        </ChipContainer>
+      </CardHeadContainer>
+      <StyledImg src='/insta-card.png' width={'100%'} height={270} />
+      <Description>{descriptionText}</Description>
+    </Container>
+  );
+};
