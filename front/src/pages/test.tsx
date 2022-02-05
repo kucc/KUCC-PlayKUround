e01d6abc445030204449232d066df6cb09d6040b
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-05 21:16:50 +0900

    참고용 test 코드 추가

--- front/src/pages/test.tsx
+++ front/src/pages/test.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
-import { SearchChipBar } from '@components';
+import { Card, SearchChipBar } from '@components';
+
+import { default as Review } from '@assets/icons/review-small.svg';
+import { default as Scrap } from '@assets/icons/scrap-small.svg';
+import { default as Star } from '@assets/icons/star-small.svg';
 
 const TestPage = () => {
-  return <SearchChipBar />;
+  const title = '인아최고 만화카페';
+  const description =
+    'No.1 만화카페에 관한 설명입니다.No.1 만화카페에 관한 설명입니다.No.1 만화카페에 관한';
+
+  const ChipGroupList = [
+    {
+      leftIcon: <Scrap />,
+      label: '123',
+    },
+    {
+      leftIcon: <Star />,
+      label: '4.9',
+    },
+    {
+      leftIcon: <Review />,
+      label: '20',
+    },
+  ];
+
+  return (
+    <div style={{ padding: '16px' }}>
+      <SearchChipBar />
+      <Card title={title} description={description} ChipGroupList={ChipGroupList} />
+    </div>
+  );
 };
 
 export default TestPage;
