ee8d580d0ccf1000b0dcae5b471ae62ed613848e
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-12 12:25:50 +0900

    기존 아이콘들을 한 번에 export 하기 위해 assets의 index.ts에 모아줬음

---
+++ front/src/assets/index.ts
@@ -0,0 +1,83 @@
+import ArrowUp from './icons/arrow-up.svg';
+import Back from './icons/back.svg';
+import Bell from './icons/bell.svg';
+import Cartoon from './icons/cartoon.svg';
+import Coffee from './icons/coffee.svg';
+import Cross from './icons/cross.svg';
+import Dice from './icons/dice.svg';
+import Down from './icons/down.svg';
+import Filter from './icons/filter.svg';
+import Global from './icons/global.svg';
+import Good from './icons/good.svg';
+import GradientCartoon from './icons/gradient-cartoon.svg';
+import GradientCoffee from './icons/gradient-coffee.svg';
+import GradientDice from './icons/gradient-dice.svg';
+import GradientGood from './icons/gradient-good.svg';
+import GradientMovie from './icons/gradient-movie.svg';
+import GradientSmile from './icons/gradient-smile.svg';
+import Hamburger from './icons/hamburger.svg';
+import HeartSmall from './icons/heart-small.svg';
+import HeartWhite from './icons/heart-white.svg';
+import Home from './icons/home.svg';
+import Location from './icons/location.svg';
+import Map from './icons/map.svg';
+import Menu from './icons/menu.svg';
+import Moon from './icons/moon.svg';
+import More from './icons/more.svg';
+import Movie from './icons/movie.svg';
+import Post from './icons/post.svg';
+import Recommend from './icons/recommend.svg';
+import ReviewSmall from './icons/review-small.svg';
+import Review from './icons/review.svg';
+import Scrap from './icons/scrap.svg';
+import Search from './icons/search.svg';
+import Setting from './icons/setting.svg';
+import Share from './icons/share.svg';
+import Smile from './icons/smile.svg';
+import StarSmall from './icons/star-small.svg';
+import Star from './icons/star.svg';
+import Sun from './icons/sun.svg';
+import Table from './icons/table.svg';
+
+export {
+  ArrowUp,
+  Back,
+  Bell,
+  Cartoon,
+  Coffee,
+  Cross,
+  Dice,
+  Down,
+  Filter,
+  Global,
+  Good,
+  GradientCartoon,
+  GradientCoffee,
+  GradientDice,
+  GradientGood,
+  GradientMovie,
+  GradientSmile,
+  Hamburger,
+  HeartSmall,
+  HeartWhite,
+  Home,
+  Location,
+  Map,
+  Menu,
+  Moon,
+  More,
+  Movie,
+  Post,
+  Recommend,
+  ReviewSmall,
+  Review,
+  Scrap,
+  Search,
+  Setting,
+  Share,
+  Smile,
+  StarSmall,
+  Star,
+  Sun,
+  Table,
+};
