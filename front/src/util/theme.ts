4825d35faf8783986c1ba3e26c6beecc68d240d5
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-13 14:08:00 +0900

    figma의 color 색상에 따라 값을 직접 넣는 것이 아니라 Colors로부터 import 해와서 사용 (아직 명확하지 않은 것은 직접 넣어줌)

--- front/src/util/theme.ts
+++ front/src/util/theme.ts
@@ -4,12 +4,11 @@ const light = {
   bg: {
     primary: Colors.white,
     grey: Colors.lightGrey,
-    black: '#1A1A18',
+    black: Colors.black,
   },
   text: {
     primary: Colors.black,
-    darkGrey: '#6B6B6B',
-    lightGrey: Colors.lightGrey,
+    darkGrey: Colors.boldGrey_6,
     white: Colors.white,
   },
   icon: {
@@ -18,9 +17,9 @@ const light = {
     yellow: Colors.yellow,
   },
   shadow: {
-    primary: '#CFCFCF',
-    dark: '#CFCFCF',
-    none: '#CFCFCF',
+    primary: Colors.shadow,
+    dark: Colors.shadow,
+    none: Colors.shadow,
   },
 };
 
@@ -28,14 +27,13 @@ const dark = {
   bg: {
     // Colors에서 수정.
     primary: Colors.black,
-    grey: '#2D2D2D',
-    black: '#FDFDFD',
+    grey: Colors.boldGrey_8,
+    black: Colors.white,
   },
   text: {
     primary: Colors.white,
-    darkGrey: '#989898',
-    lightGrey: '#2D2D2D',
-    white: Colors.white,
+    darkGrey: Colors.lightGrey_7,
+    white: Colors.black,
   },
   icon: {
     primary: Colors.primary,
@@ -43,7 +41,7 @@ const dark = {
     yellow: Colors.yellow,
   },
   shadow: {
-    primary: '#121212',
+    primary: Colors.darkShadow,
     dark: '#090909',
     none: '#232323',
   },
