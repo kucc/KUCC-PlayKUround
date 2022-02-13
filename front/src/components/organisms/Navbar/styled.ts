4d83e05c2c9dc36acd2638735ecf2c71267cc556
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-13 23:23:19 +0900

    기존 NavBar 상단에 WhiteBox 추가

--- front/src/components/organisms/Navbar/styled.ts
+++ front/src/components/organisms/Navbar/styled.ts
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
+export const WhiteBox = styled.div`
+  width: 100%;
+  height: 30px;
+`;
+
 export const StyledNavbarContainer = styled.div`
   position: relative;
+  border-bottom: 1px solid ${({ theme }) => theme.border.primary};
 `;
 
 export const StyledNavbarTextContainer = styled.div`
@@ -12,7 +18,7 @@ export const StyledNavbarTextContainer = styled.div`
 export const StyledNavbarItemsContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 22px 16px;
+  padding: 18px 16px;
 `;
 
 export const StyledNavbarItems = styled.div`
