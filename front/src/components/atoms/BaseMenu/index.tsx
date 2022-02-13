dd7332797ad179efd97153a5c3ab42eabd5274c1
mellykim123 <mellykim123@naver.com>
2022-02-13 18:16:49 +0900

    변수명 변경

--- front/src/components/atoms/BaseMenu/index.tsx
+++ front/src/components/atoms/BaseMenu/index.tsx
@@ -1,18 +1,6 @@
 import React, { useState } from 'react';
 
-import { default as Configuration } from '@assets/icons/myconfiguration.svg';
-import { default as Global } from '@assets/icons/myglobal.svg';
-import { default as MyHome } from '@assets/icons/myhome.svg';
-import { default as MyInfo } from '@assets/icons/myinfo.svg';
-import { default as Post } from '@assets/icons/mypost.svg';
-import { default as Recommend } from '@assets/icons/myrecommend.svg';
-
-import {
-  StartMenuElementWrapper,
-  StartMenuIconWrapper,
-  StartMenuLabelWrapper,
-  StartMenuWrapper,
-} from './styled';
+import { StartMenuElementWrapper, StartMenuIconWrapper, StartMenuLabelWrapper } from './styled';
 import { BaseMenuProps } from './type';
 
 export const BaseMenu = ({ clicked, width, height, label, icon, onClick }: BaseMenuProps) => {
@@ -21,8 +9,8 @@ export const BaseMenu = ({ clicked, width, height, label, icon, onClick }: BaseM
     setIsClicked(!isClicked);
   };
   return (
-    <StartMenuElementWrapper onClick={handleClick} clicked={clicked}>
-      <StartMenuIconWrapper width={width} height={height} clicked={clicked}>
+    <StartMenuElementWrapper onClick={handleClick} clicked={isClicked}>
+      <StartMenuIconWrapper width={width} height={height} clicked={isClicked}>
         {icon}
       </StartMenuIconWrapper>
       <StartMenuLabelWrapper>{label}</StartMenuLabelWrapper>
