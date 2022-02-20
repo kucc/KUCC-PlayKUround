e4ccab82739ba923ec1866f03f6f0639dc7a42fc
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-20 16:30:23 +0900

    NavBar의 각 아이콘들이 onClick 이벤트를 받을 수 있도록 변경해줌

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { HamburgerMenuWithAvatar } from '@components';
 import { Home } from '@templates';
 
 import { BellIcon, MenuIcon, SearchIcon } from '@styles';
 
 const HomePage = () => {
-  const leftItems = [<MenuIcon />];
-  const rightItems = [<SearchIcon />, <BellIcon />];
+  const [visible, setVisible] = useState<boolean>(false);
 
-  return <Home leftItems={leftItems} rightItems={rightItems} NavBarTitle='내 위치 주변' />;
+  const onClickMenuIcon = () => {
+    return setVisible(!visible);
+  };
+
+  const leftItems = [{ icon: <MenuIcon />, onClickLeftItems: onClickMenuIcon }];
+  const rightItems = [
+    { icon: <SearchIcon />, onClickRightItems: () => {} },
+    { icon: <BellIcon />, onClickRightItems: () => {} },
+  ];
+
+  return (
+    <Home
+      leftItems={leftItems}
+      rightItems={rightItems}
+      NavBarTitle='내 위치 주변'
+      visible={visible}
+      setVisible={setVisible}
+    />
+  );
 };
 
 export default HomePage;
