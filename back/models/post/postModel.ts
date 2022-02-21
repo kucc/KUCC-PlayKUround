3531083dd8eb5f36f06934fe8e727c2d65bfe48a
LHJ <jjs01hwang@gmail.com>
2022-02-21 17:50:01 +0900

    post model 생성 및 관계 생성

---
+++ back/models/post/postModel.ts
@@ -0,0 +1,49 @@
+import { DataTypes, Model, Optional, Sequelize } from 'sequelize';
+
+import { PostInterface } from './postType';
+
+// place 테이블 생성
+const PostModel = (sequelize: Sequelize) => {
+  const Post = sequelize.define<PostInterface>(
+    'post',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      placeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      likeNum: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: true,
+      },
+      sourceId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      underscored: false,
+      modelName: 'Post',
+      tableName: 'posts',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    },
+  );
+
+  return Post;
+};
+
+export default PostModel;
