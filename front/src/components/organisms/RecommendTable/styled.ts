f71f15d5b487f7a943440167e7544c1af0884fb8
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-27 01:49:23 +0900

    필요 없는 style 제거

--- front/src/components/organisms/RecommendTable/styled.ts
+++ front/src/components/organisms/RecommendTable/styled.ts
@@ -1,13 +1,11 @@
-import React from "react";
-import styled from "styled-components"
+import React from 'react';
+
+import styled from 'styled-components';
 
 export const TextWrapper = styled.div`
-font-family: Apple SD Gothic Neo;
-font-size: 16px;
-font-style: normal;
-font-weight: 700;
-line-height: 16px;
-letter-spacing: 0em;
-text-align: left;
-margin: 30px;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 16px;
+  text-align: left;
+  margin: 30px;
 `;
