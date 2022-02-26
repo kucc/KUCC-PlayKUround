40cc10ec657174fcceaec3ae7f5dc58b39fc5264
JieunKim <78676660+mellykim123@users.noreply.github.com>
2022-02-26 21:02:35 +0900

    cardwrapper 에서 card img로 변경

--- front/src/components/atoms/RecommendCard/index.tsx
+++ front/src/components/atoms/RecommendCard/index.tsx
@@ -5,8 +5,8 @@ import useWindowDimensions from '@hooks/useWindowDimensions';
 export const RecommendCard= ({imageSource, topText, bottomText, topCommaText,background,style}: RecommendCardProps) => {
     const { width, height } = useWindowDimensions();
     return (
-        <CardWrapper width = {width * 0.3} height = {width*0.3} background={background} style = {style}>
-    <CardImg src = {imageSource ||'pictures/insta-card.png'} />
+        <CardWrapper width = {width * 0.3} height = {width*0.3} style = {style}>
+    <CardImg background={background} src = {imageSource ||'pictures/insta-card.png'} />
     <CardDescription>
     <span>{topCommaText}</span>
     <div>{topText}</div>
