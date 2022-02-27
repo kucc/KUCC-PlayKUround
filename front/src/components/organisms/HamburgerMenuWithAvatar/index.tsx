408cc0a3e7ef2fa02d019dae6f8cde3f72e62027
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-27 23:47:18 +0900

    닉네임이 길 경우 ...으로 자르도록 설정

--- front/src/components/organisms/HamburgerMenuWithAvatar/index.tsx
+++ front/src/components/organisms/HamburgerMenuWithAvatar/index.tsx
@@ -88,7 +88,7 @@ export const HamburgerMenuWithAvatar = () => {
           <>
             <Div>
               <Label>
-                {me?.name} <span>님</span>
+                {me?.name.length > 6 ? me?.name.slice(0, -1) + '...' : me?.name} <span>님</span>
               </Label>
               <CursorHorizontalArrangement>
                 <FixInfo onClick={onClickMoveFixInfo}>로그인 정보 수정</FixInfo>
@@ -97,7 +97,9 @@ export const HamburgerMenuWithAvatar = () => {
             </Div>
           </>
         ) : (
-          <Label onClick={onClickLabel}>로그인 필요</Label>
+          <Div>
+            <Label onClick={onClickLabel}>로그인 필요</Label>
+          </Div>
         )}
       </InfoWrapper>
       <UnderLine />
