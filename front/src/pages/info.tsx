5ae9c1f68ad94e83e350280a141bed7e4b83ce8a
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-27 23:36:01 +0900

    info 페이지의 Modal 수정

--- front/src/pages/info.tsx
+++ front/src/pages/info.tsx
@@ -9,19 +9,20 @@ import { Info } from '@templates';
 import { loadMyInfoAPI } from 'apis/user';
 import User from 'interfaces/user';
 
+import useWindowDimensions from '@hooks/useWindowDimensions';
 import { MenuIcon } from '@styles';
 import { WRONG_LOGIN_ACCESS } from '@util/message';
 
 const InfoPage = () => {
   const { data: me, isSuccess } = useQuery<User>('user', loadMyInfoAPI);
-  const screenWidth = window.innerWidth;
+  const { width } = useWindowDimensions();
   const leftItems = [<MenuIcon />];
 
   useEffect(() => {
     if (isSuccess && !(me && me.id)) {
       Modal.error({
         content: WRONG_LOGIN_ACCESS,
-        width: `${screenWidth * 0.7}px`,
+        width: `${width * 0.7}px`,
         style: {
           top: '50%',
           transform: 'translateY(-50%)',
@@ -29,7 +30,7 @@ const InfoPage = () => {
       });
       Router.replace('/login');
     }
-  });
+  }, [me]);
 
   return me ? (
     <Info title='최근에 본 장소/코스' NavBarTitle='내 정보' leftItems={leftItems} />
