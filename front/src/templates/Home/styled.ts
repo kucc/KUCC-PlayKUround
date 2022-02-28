9cbf11169e68700a9bf50ac4d478d5ea43af3b14
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-02-28 21:34:41 +0900

    NavbarWIthHamburger 컴포넌트 import 해와서 사용

--- front/src/templates/Home/styled.ts
+++ front/src/templates/Home/styled.ts
@@ -1,22 +1,6 @@
-import { animated } from '@react-spring/web';
 import styled from 'styled-components';
 
 import { Colors } from '@styles';
-
-export const HamburgerOverlay = styled(animated.div)<{ visible: boolean }>`
-  display: block;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 999;
-`;
-
-export const HamburgerWrapper = styled(animated.div)<{ visible: boolean }>`
-  z-index: 1000;
-  position: absolute;
-`;
 
 export const FirstPageLayout = styled.div`
   display: flex;
