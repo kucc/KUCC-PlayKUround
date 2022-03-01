eaf144bbc60c569e56f44499570bbb7d2933955f
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-03-01 17:16:46 +0900

    Login templates 컴포넌트 추가

---
+++ front/src/templates/Login/styled.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const LoginLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Container = styled.div`
+  width: 260px;
+  position: absolute;
+  top: 168px;
+  text-align: center;
+`;
+
+export const InputWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-80%);
+  width: 100%;
+`;
+
+export const BoldTitle = styled.span`
+  font-family: Gmarket Sans;
+  font-size: 26px;
+  font-weight: 800;
+  line-height: 40px;
+`;
+
+export const Title = styled.span`
+  font-size: 26px;
+  line-height: 40px;
+  font-weight: 600;
+`;
+
+export const NameBoldTitle = styled.div`
+  font-size: 26px;
+  font-weight: 600;
+  line-height: 38px;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 56.16%;
+`;
