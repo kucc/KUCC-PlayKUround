2c4f4db44167218c92b5316d3aee3d4e8b63735a
JieunKim <78676660+mellykim123@users.noreply.github.com>
2022-03-02 21:15:53 +0900

    pagination bullet 색깔변경

--- front/src/components/atoms/Carousel/styled.ts
+++ front/src/components/atoms/Carousel/styled.ts
@@ -3,9 +3,21 @@ import styled from 'styled-components';
 import { Colors } from '@styles';
 
 export const SwiperContainer = styled.div`
-  > .swiper-horizontal > .swiper-pagination-bullets .swiper-pagination-bullet,
-  .swiper-pagination-horizontal.swiper-pagination-bullets .swiper-pagination-bullet {
+  .swiper-pagination-bullet {
+    width: 8px;
+    height: 8px;
+    background: ${Colors.white};
+    opacity: 1;
+  }
+  .swiper-pagination-bullet-active {
+    border-radius: 5px;
     background: ${Colors.primary};
+    border: 1px solid transparent;
+  }
+
+  .swiper-horizontal > .swiper-pagination-bullets,
+  .swiper-pagination-bullets.swiper-pagination-horizontal {
+    bottom: 30px;
   }
   img {
     width: 100%;
