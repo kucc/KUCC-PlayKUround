80498d8370706a792b4d3fd93edef0c2963fa484
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-03-06 17:51:06 +0900

    FirstSignupInput & SecondSignupInput 삭제

--- front/src/components/organisms/FirstSignupInput/type.ts
+++
@@ -1,9 +0,0 @@
-export interface FirstSignupInputProps {
-  email: string;
-  password: string;
-  passwordCheck: string;
-  onChangeEmail: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onChangePassword: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onChangePasswordCheck: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  setFirstPage: (value: boolean) => void;
-}
