b17576c30995ea2a0009eb8bd3198af959513a2d
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-03-07 21:00:57 +0900

    Fix : TypeError

--- front/src/contexts/AppContextProvider.tsx
+++ front/src/contexts/AppContextProvider.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
+
 import { combineComponents } from './combineComponents';
 import { MakeEmailProvider } from './globalEmail';
 import { SendCategoryProvider } from './sendCategory';
 
+const providers = [MakeEmailProvider, SendCategoryProvider];
 
-
-const providers = [
-  MakeEmailProvider,
-  SendCategoryProvider
-]
-
-export const AppContextProvider = combineComponents(...providers);
+export const AppContextProvider = combineComponents(...(providers as any));
