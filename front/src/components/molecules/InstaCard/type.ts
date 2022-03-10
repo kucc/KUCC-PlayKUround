0962fb50a7441804867d45b63477c3003d44ed52
LHJ <jjs01hwang@gmail.com>
2022-03-10 22:07:11 +0900

    context api 사용해서 post value 넘기기

--- front/src/components/molecules/InstaCard/type.ts
+++ front/src/components/molecules/InstaCard/type.ts
@@ -1,11 +1,17 @@
+import { commentType } from 'interfaces/comment';
+import { PlaceType } from 'interfaces/place';
+
 export type InstaCardProps = {
   titleText: string;
   placeText?: string;
   likesCount?: number;
   CarouselList: Array<any>;
   description?: string;
+  comments?: commentType[];
   isLiked?: boolean;
   userId?: number | null;
   postId?: number;
+  createdAt?: string;
+  place: PlaceType;
   setModalVisible: (value: boolean) => void;
 };
