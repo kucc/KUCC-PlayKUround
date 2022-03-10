1fbd7ba84d455802049006fb4059aec0d68c2f27
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-03-10 22:26:29 +0900

    JoinPage 완벽 구현!

--- front/src/templates/Home/index.tsx
+++ front/src/templates/Home/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import router from 'next/router';
+
 import { MainTable, NavbarWithHamburger, SearchChipBar, Text } from '@components';
 
 import {
@@ -16,17 +18,27 @@ import {
 import { HomeProps } from './type';
 
 export const Home = ({ rightItems }: HomeProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [join, setJoin] = useState<boolean>(false);
 
+  const isLocalStorgeSave = localStorage.getItem('isUserPass');
+
   const onClickJoinMainPage = () => {
+    setIsLoading(true);
+    router.push('/register').then(() => {
+      setIsLoading(false);
+    });
+    window.localStorage.setItem('isUserPass', 'pass');
+  };
+
+  const onClickNoLoginText = () => {
     setJoin(true);
     window.localStorage.setItem('isUserPass', 'pass');
   };
-  const isLocalStorgeSave = localStorage.getItem('isUserPass');
 
   return (
     <>
-      {isLocalStorgeSave === 'pass' || join ? (
+      {isLocalStorgeSave === 'pass' && join ? (
         <>
           <NavbarWithHamburger isMiddleSelect={true} rightItems={rightItems} />
           <SearchChipBar />
@@ -41,9 +53,11 @@ export const Home = ({ rightItems }: HomeProps) => {
           <Description style={{ marginTop: '24px' }}>안암, 성신여대, 혜화, 경희대 주변</Description>
           <Description>놀거리 장소 및 데이트 코스 추천 서비스</Description>
           <ButtonWrapper>
-            <StyledButton onClick={onClickJoinMainPage}>Join !</StyledButton>
+            <StyledButton height='54px' block loading={isLoading} onClick={onClickJoinMainPage}>
+              Join !
+            </StyledButton>
           </ButtonWrapper>
-          <NoLoginText>
+          <NoLoginText onClick={onClickNoLoginText}>
             로그인 없이 이용하기
             <StyledArrowRight />
           </NoLoginText>
