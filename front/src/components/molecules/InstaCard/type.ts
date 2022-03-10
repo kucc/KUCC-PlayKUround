71570896dd7bf6a726ce4c8f9915a9f2c7cf67d7
LHJ <jjs01hwang@gmail.com>
2022-03-11 00:12:39 +0900

    타입 변경

--- front/src/components/molecules/InstaCard/type.ts
+++ front/src/components/molecules/InstaCard/type.ts
@@ -1,5 +1,6 @@
 import { commentType } from 'interfaces/comment';
 import { PlaceType } from 'interfaces/place';
+import User, { ImageProps } from 'interfaces/user';
 
 export type InstaCardProps = {
   titleText: string;
@@ -9,6 +10,8 @@ export type InstaCardProps = {
   description?: string;
   comments?: commentType[];
   isLiked?: boolean;
+  userName?: string | null;
+  userImage?: ImageProps | null;
   userId?: number | null;
   postId?: number;
   createdAt?: string;
