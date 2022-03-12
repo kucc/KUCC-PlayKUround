a13d8d6265d329626ae46a61188b87f9b8a75132
LHJ <jjs01hwang@gmail.com>
2022-03-12 10:49:22 +0900

    postProfile 컴포넌트 추가

---
+++ front/src/components/molecules/PostProfile/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledProfileContainer = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+export const StyledProfileText = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+`;
