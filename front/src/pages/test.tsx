fad7270e92da486a0f6bc8aab4baa335fe8a62e2
JieunKim <78676660+mellykim123@users.noreply.github.com>
2022-03-13 18:05:52 +0900

    Update test.tsx

--- front/src/pages/test.tsx
+++ front/src/pages/test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
-import { InstaCard } from '@components';
+import { MyCourseCard } from '@components';
 import { Navbar } from '@components';
 import { MyCourseChip } from '@components';
 
+import { CourseOrderIndicator } from '@assets';
 import { Filter } from '@assets';
 import { WritePost } from '@assets';
 import { MenuIcon } from '@styles';
@@ -19,10 +20,21 @@ const TestPage = () => {
     { icon: <WritePost />, onClickRightItems: onClickMenuIcon },
     { icon: <Filter />, onClickRightItems: onClickMenuIcon },
   ];
+  const MyCourseChipList = [
+    { imageSource: 'pictures/insta-card.png', place: '안암 아줌마 아저씨 치킨' },
+    { imageSource: 'pictures/profile.png', place: '인아최고 만화카페' },
+    { imageSource: 'pictures/crying-tiger.png', place: '호랑이 술상' },
+    { imageSource: 'pictures/heart.png', place: '청년다방' },
+  ];
+  const MyCourseChipList2 = [
+    { imageSource: 'pictures/insta-card.png', place: '안암 아줌마 아저씨 치킨' },
+    { imageSource: 'pictures/profile.png', place: '인아최고 만화카페' },
+  ];
   return (
     <>
       <Navbar leftItems={leftItems} rightItems={rightItems} text='내 코스 만들기' />
-      <MyCourseChip place='안암 아줌마 아저씨 치킨' />
+      <MyCourseCard IconColor='red' MyCourseChipList={MyCourseChipList} index={1} />
+      <MyCourseCard IconColor='mint' MyCourseChipList={MyCourseChipList2} index={2} />
     </>
   );
 };
