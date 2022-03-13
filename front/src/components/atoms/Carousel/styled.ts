486e91ffcbef7b93771574f1652b2f67fab124b5
LHJ <jjs01hwang@gmail.com>
2022-03-13 22:40:22 +0900

    carousel 수정

--- front/src/components/atoms/Carousel/styled.ts
+++ front/src/components/atoms/Carousel/styled.ts
@@ -9,6 +9,7 @@ export const SwiperContainer = styled.div`
     height: 8px;
     background: ${Colors.white};
     opacity: 1;
+    border: 0.3px solid ${Colors.lightGrey_6};
   }
   .swiper-pagination-bullet-active {
     border-radius: 5px;
@@ -21,6 +22,7 @@ export const SwiperContainer = styled.div`
     position: absolute;
     top: 15rem;
   }
+
   img {
     width: 100%;
     height: 270px;
