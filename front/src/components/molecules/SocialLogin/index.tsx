4e8fb7b57d0867616aabaf88f1eb0706e5dfcf89
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-03-15 23:47:53 +0900

    front 배포 위한 준비

--- front/src/components/molecules/SocialLogin/index.tsx
+++ front/src/components/molecules/SocialLogin/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { SocialLoginIcon } from '@components';
-import { Text } from '@components';
+import { backUrl } from '@config/config';
+
+import { SocialLoginIcon, Text } from '@components';
 
 import { StyledSocialLoginContainer } from './styled';
 
@@ -12,13 +13,13 @@ export const SocialLogin = () => {
         간편 회원가입
       </Text>
       <StyledSocialLoginContainer>
-        <a href={'http://localhost:8000/api/user/google'}>
+        <a href={`${backUrl}/api/user/google`}>
           <SocialLoginIcon type='google' />
         </a>
-        <a href={'http://localhost:8000/api/user/naver'}>
+        <a href={`${backUrl}/api/user/naver`}>
           <SocialLoginIcon type='naver' />
         </a>
-        <a href={'http://localhost:8000/api/user/kakao'}>
+        <a href={`${backUrl}/api/user/kakao`}>
           <SocialLoginIcon type='kakao' />
         </a>
       </StyledSocialLoginContainer>
--- front/src/config/config.ts
+++ front/src/config/config.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const backUrl =
-  process.env.NODE_ENV === 'production' ? 'http://54.180.107.3' : 'http://localhost:8000';
+  process.env.NODE_ENV === 'production' ? 'http://13.209.95.126/' : 'http://localhost:8000';
