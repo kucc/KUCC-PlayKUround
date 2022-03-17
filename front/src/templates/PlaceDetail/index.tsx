86f24d7b0750334df9a155752b9d6de30252736f
LHJ <jjs01hwang@gmail.com>
2022-03-17 23:29:30 +0900

    feature/ PlaceDetailComponent 추가

---
+++ front/src/templates/PlaceDetail/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+import { Skeleton } from 'antd';
+
+import { BackIconWithNavbar, Carousel, PlaceInfo } from '@components';
+
+import { getByOneAPI } from 'apis/place';
+import { PlaceType } from 'interfaces/place';
+
+import { placeDetailProps } from './type';
+
+export const PlaceDetail: React.FC<placeDetailProps> = ({ placeId }) => {
+  // fetch PlaceInfo
+  const { data, isLoading, isSuccess } = useQuery<PlaceType>(['place', placeId], getByOneAPI, {
+    enabled: !!placeId,
+  });
+
+  const place = data as PlaceType;
+
+  if (!placeId || isLoading) return <Skeleton />;
+
+  const { placeName, placeDescription, ratingNumber, commentCount, scrapCount } = place;
+
+  return (
+    <div>
+      <BackIconWithNavbar text={''} />
+      <PlaceInfo
+        placeName={placeName}
+        placeDescription={placeDescription}
+        ratingNumber={ratingNumber}
+        commentCount={commentCount}
+        scrapCount={scrapCount}
+      />
+      <Carousel CarouselList={place.images} height={190} />
+    </div>
+  );
+};
