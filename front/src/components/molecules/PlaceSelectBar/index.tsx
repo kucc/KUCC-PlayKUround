1c26a36a49edae06ceda797abb7e6030fa4bfbaa
LHJ <jjs01hwang@gmail.com>
2022-03-18 14:44:24 +0900

    feature/ PlaceSelectBar component 생성

---
+++ front/src/components/molecules/PlaceSelectBar/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Chip } from '@components';
+
+import { ChipStyle, StyledPlaceSelectContainer } from './styled';
+import { PlaceSelectBarProps } from './type';
+
+const lableList = ['홈', '메뉴', '별점 및 리뷰', '지도'];
+
+// clicked={selectedIndex === index}
+
+export const PlaceSelectBar: React.FC<PlaceSelectBarProps> = ({
+  setSelectedIndex,
+  selectedIndex,
+}) => {
+  return (
+    <StyledPlaceSelectContainer>
+      {lableList.map((lable, index) => (
+        <Chip
+          label={lable}
+          onClick={() => setSelectedIndex(index)}
+          clickable={true}
+          clicked={selectedIndex === index}
+          key={index}
+          style={ChipStyle}
+        />
+      ))}
+    </StyledPlaceSelectContainer>
+  );
+};
