61964b49886fab8c3dbc945f7bd952b4e95676fe
LHJ <jjs01hwang@gmail.com>
2022-03-18 14:44:43 +0900

    feature/ SendDistance context 생성

--- front/src/contexts/AppContextProvider.tsx
+++ front/src/contexts/AppContextProvider.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import { combineComponents } from './combineComponents';
+import { DistanceValueProvider } from './distanceValue';
 import { filterValueProvider } from './filterValue';
 import { MakeEmailProvider } from './globalEmail';
 import { postValueProvider } from './postValue';
 
-const providers = [MakeEmailProvider, filterValueProvider, postValueProvider];
+const providers = [
+  MakeEmailProvider,
+  filterValueProvider,
+  postValueProvider,
+  DistanceValueProvider,
+];
 
 export const AppContextProvider = combineComponents(...providers);
